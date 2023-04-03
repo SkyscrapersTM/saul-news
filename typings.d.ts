990442bc633b0f0d33223daf90dd7359e167a830
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-04-03 13:23:34 -0500

    feat(Root): Added news type for typescript

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,28 @@
+type DataEntry = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+};
+
+type Pagination = {
+  count: Int;
+  limit: Int;
+  offset: Int;
+  total: Int;
+};
+
+type NewsResponse = {
+  data: DataEntry[];
+  pagination: Pagination;
+};
+
 type Category =
   | "general"
   | "business"
