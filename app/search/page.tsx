207e319089728f5ada000e4ff6c7b32ed1f0e134
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-04-04 12:33:30 -0500

    feat(Root): Added search page

---
+++ app/search/page.tsx
@@ -0,0 +1,22 @@
+import fetchNews from "util/fetchNews";
+import NewsList from "app/components/NewsList";
+
+type Props = {
+  searchParams?: { term: string };
+};
+
+const SearchPage = async ({ searchParams }: Props) => {
+  const news: NewsResponse = await fetchNews(
+    "general",
+    searchParams?.term,
+    true
+  );
+  return (
+    <div>
+      <h1 className="headerTitle">Search Results for: {searchParams?.term}</h1>
+      <NewsList news={news} />
+    </div>
+  );
+};
+
+export default SearchPage;
