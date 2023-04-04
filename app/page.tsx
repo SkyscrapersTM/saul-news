5ac1a9dd30aa12488e10f21f65fc4c3984407ddb
SkyscrapersTM <74199653+SkyscrapersTM@users.noreply.github.com>
2023-04-04 12:22:17 -0500

    feat(Root): Added NewList component in main page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main>Home</main>;
-}
+import { categories } from "constants-k";
+import fetchNews from "util/fetchNews";
+import NewsList from "app/components/NewsList";
+
+const Home = async () => {
+  // fetch the news data
+  const news: NewsResponse = await fetchNews(categories.join(","));
+
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
+};
+
+export default Home;
